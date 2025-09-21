420a95fbc6f291d874f1b8faa88331c64e5380de
kosiedoziem <edoziemsomto@gmail.com>
2025-09-21 00:17:28 -0400

    dash

--- src/lib/components/PatientDashboard.svelte
+++ src/lib/components/PatientDashboard.svelte
@@ -3,10 +3,10 @@
     import DashboardNavElement from "./DashboardNavElement.svelte";
     import ProfileForm from "./ProfileForm.svelte";
 </script>
-
-Patient Dashboard
-<div class="flex">
-    <DashboardNavElement title="View your profile" />    
-    <DashboardNavElement />
-    <DashboardNavElement />
-</div>
+<body class="flex flex-col">
+    <div class="flex flex-col">
+        <DashboardNavElement title="View your profile" subtext="Access your information & medical history" buttonText="Edit"/>    
+        <DashboardNavElement title="Communicate with your provider" subtext="Stay connected with your healthcare team" buttonText="Start Chat"/>
+        <DashboardNavElement title="Update Medical Events" subtext="Input critical information to keep your team up to date" buttonText="Add a new event"/>
+    </div>
+</body>
