fa68eaa70b848deb566879c0db20d7890454e977
Ruadhan KellerMcLeer <ruadhan.kellermcleer@gmail.com>
2025-09-21 06:01:26 -0400

    chat css

--- src/routes/dashboard/communication/[slug]/chat/+page.svelte
+++ src/routes/dashboard/communication/[slug]/chat/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 <Navbar/>
 <h1 class="text-[#286480] text-3xl font-semibold py-10 px-15">Messaging</h1>
-<div class="flex flex-col items-center justify-center h-screen bg-cover bg-center">
+<div class="flex flex-col items-center h-screen bg-cover bg-center">
     <Chat></Chat>
 </div>
 <Footer/>
