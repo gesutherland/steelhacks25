734caa08ba69ac99902adba4a6242d0ada591b2f
Gabrielle Sutherland <164038519+gesutherland@users.noreply.github.com>
2025-09-21 04:37:15 -0400

    updateeee

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,21 +2,19 @@
     import { goto } from "$app/navigation";
     import NavbarItem from "$lib/components/NavbarItem.svelte";
     let imagePath = "/ourchartlogo.svg";
-
 </script>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&display=swap" rel="stylesheet">
 
-
 <div class="flex flex-row justify-between px-15 items-center h-15" >
-    <img class="w-39" src={imagePath} alt="OurChart logo" on:click={() => goto('/')}/>
+    <img class="w-39 cursor-pointer" src={imagePath} alt="OurChart logo" on:click={() => goto('/')}/>
     <div class="flex flex-row gap-x-4 font-large font-sans text-[#286480]">
-        <NavbarItem on:click={goto("/")}>Home</NavbarItem>
-        <NavbarItem on:click={goto("/providers")}>Find Your Provider</NavbarItem>
-        <NavbarItem on:click={goto("/support")}>Support</NavbarItem>
-        <NavbarItem on:click={goto("/login")}>Login</NavbarItem>
-        <NavbarItem on:click={goto("/logout")}>Logout</NavbarItem>
+        <NavbarItem on:click={() => goto("/")}>Home</NavbarItem>
+        <NavbarItem on:click={() => goto("/providers")}>Find Your Provider</NavbarItem>
+        <NavbarItem on:click={() => goto("/support")}>Support</NavbarItem>
+        <NavbarItem on:click={() => goto("/login")}>Login</NavbarItem>
+        <NavbarItem on:click={() => goto("/")}>Logout</NavbarItem>
     </div>
 </div>
