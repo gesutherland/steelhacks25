dd10779a5ea6c90d4d2f57ad8126f349d11ba920
Gabrielle Sutherland <164038519+gesutherland@users.noreply.github.com>
2025-09-21 04:44:52 -0400

    trying to update so maybe it links now?

--- src/lib/components/NavbarItem.svelte
+++ src/lib/components/NavbarItem.svelte
@@ -2,7 +2,6 @@
     export let url;
 </script>
 
-<button 
-    class="hover:text-blue-600 transition-colors cursor-pointer bg-transparent border-none font-inherit text-inherit p-0"
-    on:click={() => goto(url)}
-><slot></slot></button>
+<a href={url} class="hover:text-blue-600 transition-colors cursor-pointer">
+    <slot></slot>
+</a>
