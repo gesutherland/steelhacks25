8973d0c352bd02bd5faaebfd331a7a41f22f6ef5
Gabrielle Sutherland <164038519+gesutherland@users.noreply.github.com>
2025-09-21 04:33:47 -0400

    too many s's

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -24,7 +24,7 @@
                 goto('/');
         }
     }
-</sscript>
+</script>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
