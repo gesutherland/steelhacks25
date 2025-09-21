82f973930667d95e053656c8aae259d3c614fc51
kosiedoziem <edoziemsomto@gmail.com>
2025-09-21 02:07:53 -0400

    full provider dash

--- src/lib/components/ProviderDashboard.svelte
+++ src/lib/components/ProviderDashboard.svelte
@@ -1,7 +1,12 @@
 <script>
     import DashboardNavElement from "./DashboardNavElement.svelte";
+    import Navbar from "./Navbar.svelte";
+    import Footer from "./Footer.svelte";
 </script>
+<Navbar/>
+<div class="flex flex-col items-center justify-center h-screen bg-cover bg-center" style="background-image: url('/unsplash_pKpipZVVEC0.png')">
 
-Provider Dashboard
-<DashboardNavElement title="View Patient Profiles"></DashboardNavElement>
-<DashboardNavElement title="View Patient Events"></DashboardNavElement>
+<DashboardNavElement title="View Patient Profiles" subtext="Access list of patient profiles" imageUrl="/profilepic.svg" buttonText="View Patients"></DashboardNavElement>
+<DashboardNavElement title="View Patient Events" subtext="Access urgent patient information" imageUrl="/Mask group2.svg" buttonText="View Events"></DashboardNavElement>
+</div>
+<Footer/>
