19a4355626e3f13d23340c4a11f47314760c3371
Ruadhan KellerMcLeer <ruadhan.kellermcleer@gmail.com>
2025-09-21 01:14:41 -0400

    merge

--- src/lib/components/PatientDashboard.svelte
+++ src/lib/components/PatientDashboard.svelte
@@ -5,15 +5,6 @@
     import Navbar from "./Navbar.svelte";
     import Footer from "./Footer.svelte";
 </script>
-<<<<<<< HEAD
-<Navbar/>
-<div class="flex flex-col items-center justify-center h-screen bg-cover bg-center" style="background-image: url('/unsplash_pKpipZVVEC0.png')">
-    <DashboardNavElement imageUrl="/profilepic.svg" title="View your profile" subtext="Access your information & medical history" buttonText="Continue"/>   
-    <DashboardNavElement imageUrl="/Mask group.svg" title="Communicate with your provider" subtext="Stay connected with your healthcare team" buttonText="Start Chat"/>
-    <DashboardNavElement imageUrl="/Mask group2.svg" title="Update Medical Events" subtext="Input critical information to keep your team up to date" buttonText="Add a new event"/>
-</div>
-<Footer/>
-=======
 <div class="flex flex-col">
     <div class="flex flex-col">
         <DashboardNavElement title="View your profile" subtext="Access your information & medical history" buttonText="Edit"></DashboardNavElement>    
@@ -21,4 +12,3 @@
         <DashboardNavElement title="Update Medical Events" subtext="Input critical information to keep your team up to date" buttonText="Add a new event"></DashboardNavElement>
     </div>
 </div>
->>>>>>> 7426e42796d10f8d4d706df90d57633fc63cf5de
