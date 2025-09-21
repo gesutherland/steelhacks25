eb652c02b0e20752044ef0acc4a242f395b01807
kosiedoziem <edoziemsomto@gmail.com>
2025-09-20 22:38:42 -0400

    starting login page

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -26,14 +26,17 @@
     }
 </script>
 
-<div>
-    <input type="radio" name="userType" id="patients" value="patients" checked={autoUserType=="patients"?"checked":""} on:change={(v) => selectedUserType = "patients"}/>
+<div class="flex flex-col bg-white p-10 rounded-lg shadow-lg gap-4">
+    <div class="bg-blue-500 p-4 rounded-lg text-white flex justify-center items-center">
+        <img src="/ourchartlogo.svg" alt="OurChart Logo" class="w-32 mb-4"/>
+    </div>
+    <input class="checked:bg-black" type="radio" name="userType" id="patients" value="patients" checked={autoUserType=="patients"?"checked":""} on:change={(v) => selectedUserType = "patients"}/>
     <label for="patients">I'm a Patient</label>
-    <input type="radio" name="userType" id="providers" value="providers" checked={autoUserType=="providers"?"checked":""} on:change={(v) => selectedUserType = "providers"}/>
+    <input class="checked:bg-black" type="radio" name="userType" id="providers" value="providers" checked={autoUserType=="providers"?"checked":""} on:change={(v) => selectedUserType = "providers"}/>
     <label for="providers">I'm a Provider</label>
-</div>
 
-<input bind:value={email} type="email" />
-<input bind:value={password} type="password" />
-<button on:click={signUp}>Sign up</button>
-<button on:click={logIn}>Log in</button>
+    <input class="border-2 border-gray-300 p-2" bind:value={email} type="email" />
+    <input class="border-2 border-gray-300 p-2" bind:value={password} type="password" />
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={signUp}>Sign up</button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={logIn}>Log in</button>
+</div>
